00ef999fd38a965f8d89b6e3453c03f1c8f5576a
Jules Immerzeel <jules.immerzeel@gmail.com>
2021-04-09 00:41:39 +0200

    Add ddns to allowed host

--- doodler/settings_development.py
+++ doodler/settings_development.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z06o6jifmfxj@u4i!1ardq1vu+t4n2gfr#(ivy7*2+6mz7#e!c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.2.22', 'localhost']
+ALLOWED_HOSTS = ['julesimmerzeel.nl', 'immerzeel15.ddns.net', '192.168.2.19']
 
 
 # Application definition
