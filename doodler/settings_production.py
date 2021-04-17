d1913ca953cf6ca7c0aaf9d2e174f0a41691577f
Jules Immerzeel <jules.immerzeel@gmail.com>
2021-04-17 12:53:45 +0200

    Fix static files in production

--- doodler/settings_production.py
+++ doodler/settings_production.py
@@ -25,12 +25,13 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'z06o6jifmfxj@u4i!1ardq1vu+t4n2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['julesimmerzeel.com', '192.168.2.19', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,3 +136,4 @@ CSRF_COOKIE_SECURE = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
